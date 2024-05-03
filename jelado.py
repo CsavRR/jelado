72d8af0cf64faac060703ecbef12fe74386f7370
Csav_R <116081238+CsavRR@users.noreply.github.com>
2024-05-03 11:26:00 +0200

    2. feladat

--- jelado.py
+++ jelado.py
@@ -10,10 +10,13 @@ class Adat:
         return f'Óra: {self.ora} \nPerc: {self.perc} \nMásodperc: {self.mperc} \nX: {self.x} \nY: {self.y}'
     
 f = open('jel.txt', 'rt', encoding='UTF-8')
-f.readline()
 
 lista = []
 for sor in f:
     sor = sor.strip().split(' ')
     lista.append(Adat(sor[0], sor[1], sor[2], sor[3], sor[4]))
 
+print('2. feladat')
+beker = int(input('Adja meg a jel sorszámát! ')) - 1
+
+print(f'x={lista[beker].x} y={lista[beker].y}')
